fa5acbcddebe04f7872afdab0fdef143a95c4f61
shashankjo <kumarshashank.jo@gmail.com>
2024-07-15 22:49:00 +0530

    Create a basic todo-list without using Hotwire

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: "Task was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def task_params
+    params.permit(:description)
+  end
 end
