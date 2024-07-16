bec1b8c26398f31bb8df6662416d876c5a644c39
Kumar Shashank <40141542+shashankjo@users.noreply.github.com>
2024-07-16 12:10:45 +0530

    Create a basic todo-list without using Hotwire (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    root "tasks#index"
+
+    resources :tasks
+  end
 end
