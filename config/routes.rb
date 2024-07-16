d0bd25d3a18efe81f92fcca08b831e456ed47c9f
shashankjo <kumarshashank.jo@gmail.com>
2024-07-16 16:32:39 +0530

    Create tasks using Stimulus

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   Rails.application.routes.draw do
     root "tasks#index"
+    post "tasks/:id/toggle", to: "tasks#toggle"
 
     resources :tasks
   end
